d7e31b81dd439fde53e57e72270ce44e032907b5
Adelemphii <demilitioowner@gmail.com>
2021-05-20 13:09:43 -0500

    Procfile

--- src/main/java/me/adelemphii/adelem/Core.java
+++ src/main/java/me/adelemphii/adelem/Core.java
@@ -1,19 +1,15 @@
 package me.adelemphii.adelem;
 
 import me.adelemphii.adelem.commands.ServerStatusCommands;
-import me.adelemphii.adelem.util.MineStats;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
 public class Core {
 
-    // Make this secure later
-    private static final String TOKEN = "ODQ0OTU3NTQ4MTM0MDA2ODA2.YKZ-Cg.GyuyndNFkB0NsuFClz1-qj2MIVI";
-
     public static void main(String[] args) {
 
         DiscordApi api = new DiscordApiBuilder()
-                .setToken(TOKEN)
+                .setToken(System.getenv("TOKEN"))
                 .login().join();
 
         api.addListener(new ServerStatusCommands());
